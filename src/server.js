62b85bc5a56d2141dad89124349ae0229de24afa
InspiredEnigma12477 <sakoreshivam3901@gmail.com>
2022-12-28 15:33:43 +0530

    node app

---
+++ src/server.js
@@ -0,0 +1,12 @@
+//create a server
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {    
+    res.send('Hello World Shivam this side!');
+}); 
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+}) 
